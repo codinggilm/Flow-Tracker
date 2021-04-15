ff54b6532934048a360774864de72dd22598d379
coddinggilm <54217958+codinggilm@users.noreply.github.com>
2021-04-15 20:40:11 +0100

    deploying client v1.0

--- src/redux/apis/flowApi.js
+++ src/redux/apis/flowApi.js
@@ -3,5 +3,6 @@ import axios from 'axios';
 
 export default axios.create({
     // baseURL: 'http://localhost:3000'
-    baseURL: 'https://lit-woodland-91230.herokuapp.com'
+    // baseURL: 'https://lit-woodland-91230.herokuapp.com'
+    baseURL: 'https://flow-tracker-api.herokuapp.com'
 })
